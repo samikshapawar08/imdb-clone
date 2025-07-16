4caa12a5e4ba65db0f081f9cb093ab4e92508e38
samiksha pawar <sameekshapawar06@gmail.com>
2025-07-16 18:52:18 +0530

    created header

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,36 +1,98 @@
-import React from 'react'
-import {AppBar, Toolbar, styled, Box} from '@mui/material';
-import {logoURL} from '../../constants/constant';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Box,
+  Typography,
+  InputBase,
+} from '@mui/material';
+import { logoURL } from '../../constants/constant';
+import { Menu, BookmarkAdd, ExpandMore } from '@mui/icons-material';
 
+import HeaderMenu from './HeaderMenu';
 
 const StyleToolBar = styled(Toolbar)`
   background-color: #121212;
   min-height: 56px !important;
-  padding: 0 115px !important`
+  padding: 0 115px !important;
+  justify-content: space-between;
 
-  const Logo = styled('img')({
-    width:64
-  })
+  & > * {
+    padding: 0 16px;
+    color: white;
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    & > p {
+      font-size: 14px;
+      font-weight: 600;
+      margin-left: 5px;
+    }
+  }
+`;
+
+const InputSearchField = styled(InputBase)`
+  background-color: #ffffff;
+  height: 30px;
+  width: 500px;
+  border-radius: 5px;
+  padding: 0 10px;
+`;
+
+const Logo = styled('img')({
+  width: 80 
+});
 
 function Header() {
+  const [open, setOpen] = useState(null);
+
+  const handleClick = (e) => {
+    setOpen(e.currentTarget); // ✅ fixed case-sensitive issue
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
   return (
     <AppBar>
-        <StyleToolBar>
-            <Logo src={logoURL} alt="IMDB Logo"  />
-            <Box>
-                <MenuIcon />
-            </Box>
+      <StyleToolBar>
+        <Logo src={logoURL} alt="IMDB Logo" />
 
-        </StyleToolBar>    
+        <Box onClick={handleClick}>
+          <Menu />
+          <Typography>Menu</Typography>
+        </Box>
+
+        <HeaderMenu open={open} handleClose={handleClose} />
+        <InputSearchField placeholder="Search…" />
+
+        <Typography component="span">
+          IMDb{' '}
+          <Box component="span" sx={{ color: '#f5c518' }}>
+            Pro
+          </Box>
+        </Typography>
+
+        <Box>
+          <BookmarkAdd />
+          <Typography>Watchlist</Typography>
+        </Box>
+
+        <Typography>Sign In</Typography>
+
+        <Box>
+          <Typography>EN</Typography>
+          <ExpandMore />
+        </Box>
+      </StyleToolBar>
     </AppBar>
-  )
+  );
 }
 
-export default Header
-
-
-
-
-
-
+export default Header;
